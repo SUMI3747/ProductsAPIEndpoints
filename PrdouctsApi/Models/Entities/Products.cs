4785832ea53ca471b6a315dcc2d28574233815fb
SUMI3747 <sumitsl3747@gmail.com>
2024-11-27 20:14:18 +0530

    Authentication first step

--- PrdouctsApi/Models/Entities/Products.cs
+++ PrdouctsApi/Models/Entities/Products.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PrdouctsApi.Models.Entities
 {
+    //Product Table coulums
     public class Products: IProducts
     {
         [Required]
